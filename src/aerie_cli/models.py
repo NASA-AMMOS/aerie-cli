3797554b869272672cf761e2ab73cd07df4220d2
Mihir Kamble <mihir.m.kamble@jpl.nasa.gov>
2022-02-28 13:09:42 -0600

    model tree: upload()

---
+++ src/aerie_cli/models.py
@@ -0,0 +1,40 @@
+import json
+from typing import Union
+
+import typer
+import arrow
+
+from .client import AerieClient
+from .client import Auth
+from .schemas.client import ActivityPlanCreate
+
+app = typer.Typer()
+
+@app.command
+def upload(
+    username: str = typer.Option(..., help="JPL username", prompt=True),
+    password: str = typer.Option(
+        ...,
+        help="JPL password",
+        prompt=True,
+        hide_input=True,
+    ),
+    server_url: str = typer.Option(
+        "http://localhost", help="The URL of the Aerie deployment"
+    ),
+    mission_model_path: str = typer.Option(
+        ..., help="The input file from which to create an Aerie model", prompt=True
+    ),
+    model_name: str = typer.Option(
+        ..., help="Name of mission model", prompt = True
+    )
+):
+    auth = Auth(username, password)
+    client = AerieClient(server_url=server_url, auth=auth)
+    
+    # Upload mission model file to Aerie server
+    model_id = client.upload_mission_model(mission_model_path=mission_model_path)
+
+    typer.echo(f"Created new mission model: {model_name} at {client.ui_path()}/models with Model ID: {model_id}")
+
+
