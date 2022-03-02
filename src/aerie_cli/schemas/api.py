62bd1ed9c9b9d6dfe47815fcc07d6db69f6a707c
gavinm <gavin.c.martin@jpl.nasa.gov>
2022-03-01 18:05:34 -0800

    Added simulation result schemas to system

--- src/aerie_cli/schemas/api.py
+++ src/aerie_cli/schemas/api.py
@@ -2,6 +2,7 @@ import json
 from dataclasses import dataclass
 from datetime import timedelta
 from typing import Any
+from typing import Optional
 
 import arrow
 from arrow import Arrow
@@ -65,4 +66,70 @@ class ApiActivityPlanRead(ApiActivityPlanCreate):
                 ApiActivityRead.from_dict(activity_dict)
                 for activity_dict in obj["activities"]
             ],
+        )
+
+
+@dataclass
+class ApiAsSimulatedActivity:
+    type: str
+    parent: Optional[str]
+    start_timestamp: Arrow
+    children: list[str]
+    duration: int  # microseconds
+    parameters: dict[str, Any]
+
+    @classmethod
+    def from_json(cls, json_str: str) -> "ApiAsSimulatedActivity":
+        return ApiAsSimulatedActivity.from_dict(json.loads(json_str))
+
+    @classmethod
+    def from_dict(cls, obj: dict) -> "ApiAsSimulatedActivity":
+        print(obj)
+        return ApiAsSimulatedActivity(
+            type=obj["type"],
+            parent=obj["parent"],
+            startTimestamp=arrow.get(obj["startTimestamp"]),
+            children=obj["children"],
+            duration=obj["duration"],
+            parameters=obj["parameters"],
+        )
+
+
+@dataclass
+class ApiSimulatedResourceSample:
+    x: int  # microseconds since profile start
+    y: Any
+
+
+@dataclass
+class ApiSimulationResults:
+    start: Arrow
+    activities: dict[str, ApiAsSimulatedActivity]
+    # TODO: implement constraints
+    constraints: Any
+    resources: dict[str, list[ApiSimulatedResourceSample]]
+    # TODO: implement events
+    events: Any
+
+    @classmethod
+    def from_json(cls, json_str: str) -> "ApiSimulationResults":
+        return ApiSimulationResults.from_dict(json.loads(json_str))
+
+    @classmethod
+    def from_dict(cls, obj: dict) -> "ApiSimulationResults":
+        return ApiSimulationResults(
+            start=arrow.get(obj["start"]),
+            activities={
+                id: ApiAsSimulatedActivity.from_dict(act)
+                for id, act in obj["activities"].items()
+            },
+            constraints=obj["constraints"],
+            resources={
+                name: [
+                    ApiSimulatedResourceSample(x=sample["x"], y=sample["y"])
+                    for sample in samples
+                ]
+                for name, samples in obj["resources"].items()
+            },
+            events=obj["events"],
         )
