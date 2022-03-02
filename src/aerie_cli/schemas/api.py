624c09cea184afb1d02a0c57552385e734143869
gavinm <gavin.c.martin@jpl.nasa.gov>
2022-03-01 18:50:38 -0800

    Cleaned up API deserialization code

--- src/aerie_cli/schemas/api.py
+++ src/aerie_cli/schemas/api.py
@@ -1,135 +1,71 @@
-import json
 from dataclasses import dataclass
-from datetime import timedelta
+from dataclasses import field
 from typing import Any
 from typing import Optional
 
-import arrow
-from arrow import Arrow
+from dataclasses_json import config
+from dataclasses_json import dataclass_json
+from dataclasses_json import LetterCase
 
-from ..utils.serialization import hms_string_to_timedelta
 
-
+@dataclass_json
 @dataclass
 class ApiActivityCreate:
     type: str
     plan_id: int
-    start_offset: timedelta
+    start_offset: str  # HMS string
     arguments: dict[str, Any]
 
 
+@dataclass_json
 @dataclass
 class ApiActivityRead(ApiActivityCreate):
     id: int
 
-    @classmethod
-    def from_json(cls, json_str: str) -> "ApiActivityRead":
-        return ApiActivityRead.from_dict(json.loads(json_str))
 
-    @classmethod
-    def from_dict(cls, obj: dict) -> "ApiActivityRead":
-        return ApiActivityRead(
-            type=obj["type"],
-            plan_id=obj["plan_id"],
-            start_offset=hms_string_to_timedelta(obj["start_offset"]),
-            arguments=obj["arguments"],
-            id=obj["id"],
-        )
-
-
+@dataclass_json
 @dataclass
 class ApiActivityPlanCreate:
     model_id: int
     name: str
-    start_time: Arrow
-    duration: timedelta
+    start_time: str  # ISO format
+    duration: str  # HMS string
 
 
+@dataclass_json
 @dataclass
 class ApiActivityPlanRead(ApiActivityPlanCreate):
     id: int
     activities: list[ApiActivityRead]
 
-    @classmethod
-    def from_json(cls, json_str: str) -> "ApiActivityPlanRead":
-        return ApiActivityPlanRead.from_dict(json.loads(json_str))
 
-    @classmethod
-    def from_dict(cls, obj: dict) -> "ApiActivityPlanRead":
-        return ApiActivityPlanRead(
-            id=obj["id"],
-            model_id=obj["model_id"],
-            name=obj["name"],
-            start_time=arrow.get(obj["start_time"]),
-            duration=hms_string_to_timedelta(obj["duration"]),
-            activities=[
-                ApiActivityRead.from_dict(activity_dict)
-                for activity_dict in obj["activities"]
-            ],
-        )
-
-
+@dataclass_json
 @dataclass
 class ApiAsSimulatedActivity:
     type: str
     parent: Optional[str]
-    start_timestamp: Arrow
+    start_timestamp: str = field(
+        metadata=config(letter_case=LetterCase.CAMEL)
+    )  # ISO format
     children: list[str]
-    duration: int  # microseconds
+    duration: int  # microseconds since plan start
     parameters: dict[str, Any]
 
-    @classmethod
-    def from_json(cls, json_str: str) -> "ApiAsSimulatedActivity":
-        return ApiAsSimulatedActivity.from_dict(json.loads(json_str))
 
-    @classmethod
-    def from_dict(cls, obj: dict) -> "ApiAsSimulatedActivity":
-        print(obj)
-        return ApiAsSimulatedActivity(
-            type=obj["type"],
-            parent=obj["parent"],
-            startTimestamp=arrow.get(obj["startTimestamp"]),
-            children=obj["children"],
-            duration=obj["duration"],
-            parameters=obj["parameters"],
-        )
-
-
+@dataclass_json
 @dataclass
 class ApiSimulatedResourceSample:
-    x: int  # microseconds since profile start
+    x: int  # microseconds since plan start
     y: Any
 
 
+@dataclass_json
 @dataclass
 class ApiSimulationResults:
-    start: Arrow
+    start: str
     activities: dict[str, ApiAsSimulatedActivity]
     # TODO: implement constraints
     constraints: Any
     resources: dict[str, list[ApiSimulatedResourceSample]]
     # TODO: implement events
     events: Any
-
-    @classmethod
-    def from_json(cls, json_str: str) -> "ApiSimulationResults":
-        return ApiSimulationResults.from_dict(json.loads(json_str))
-
-    @classmethod
-    def from_dict(cls, obj: dict) -> "ApiSimulationResults":
-        return ApiSimulationResults(
-            start=arrow.get(obj["start"]),
-            activities={
-                id: ApiAsSimulatedActivity.from_dict(act)
-                for id, act in obj["activities"].items()
-            },
-            constraints=obj["constraints"],
-            resources={
-                name: [
-                    ApiSimulatedResourceSample(x=sample["x"], y=sample["y"])
-                    for sample in samples
-                ]
-                for name, samples in obj["resources"].items()
-            },
-            events=obj["events"],
-        )
