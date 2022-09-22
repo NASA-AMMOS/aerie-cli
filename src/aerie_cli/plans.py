b272cefb9753a0991e9ab413984ab49752a96cd8
Josh Haug <jhaug@jpl.nasa.gov>
2022-09-22 11:03:07 -0700

    Changed to use built-in graphql query method

--- src/aerie_cli/plans.py
+++ src/aerie_cli/plans.py
@@ -134,26 +134,15 @@ def simulate(
 
     typer.echo(f"Simulating activity plan at: {client.ui_path()}/plans/{id}")
     sim_dataset_id = client.simulate_plan(id, poll_period)
+    res = client.get_simulation_results(id, sim_dataset_id)
     
-
-    # if output:
-    #     api_resource_timeline = client.get_resource_timelines(id)
-    # else:
-    #     api_resource_timeline = ApiResourceSampleResults(resourceSamples={})
-
-    # TODO: Fix simulation results to include activity information
-    # results = SimulationResults.
-    # from_api_results(sim_dataset_id, api_resource_timeline)
-
-    res = client.get_simulation_results(id)
-    typer.echo(f"Simulated activity plan at: {client.ui_path()}/plans/{id}")
-    typer.echo(f"SimDatasetId: {sim_dataset_id}")
+    #typer.echo(f"Simulated activity plan at: {client.ui_path()}/plans/{id}")
+    #typer.echo(f"SimDatasetId: {sim_dataset_id}")
             
     if output:
-        typer.echo("Writing simulation results...")
+        typer.echo("Writing simulation results to {output}")
         with open(output, "w") as out_file:
             out_file.write(json.dumps(res, indent=2))
-        typer.echo(f"Wrote results to {output}")
 
 
 @app.command()
