a1413c2a36086a55d04ddbfee2ac824b742d9581
Jonathan J Navarro <navarrojonathanj@gmail.com>
2023-06-29 11:53:04 -0700

    Add CommandContext.get_client()

--- src/aerie_cli/commands/command_context.py
+++ src/aerie_cli/commands/command_context.py
@@ -1,4 +1,6 @@
 import typer
+from aerie_cli.aerie_client import AerieClient
+from aerie_cli.utils.sessions import get_active_session_client
 
 app = typer.Typer()
 
@@ -6,4 +8,10 @@ class CommandContext:
     hasura_admin_secret = None
     def __init__(self) -> None:
         raise NotImplementedError
-        
+
+    @classmethod
+    def get_client(cls) -> AerieClient:
+        client = get_active_session_client()
+        if CommandContext.hasura_admin_secret:
+            client.host_session.session.headers["x-hasura-admin-secret"] = CommandContext.hasura_admin_secret
+        return client
