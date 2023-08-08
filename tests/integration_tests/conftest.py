f3ea046b0d68e7d36c25667a4281bfabda820529
Jonathan J Navarro <Jonathan.J.Navarro@jpl.nasa.gov>
2023-08-08 13:30:16 -0700

    Set up configuration in conftest

--- tests/integration_tests/conftest.py
+++ tests/integration_tests/conftest.py
@@ -10,7 +10,6 @@ from aerie_cli.aerie_host import AerieHostSession, AuthMethod
 from aerie_cli.commands.configurations import delete_all_persistent_files, upload_configurations, deactivate_session, activate_session
 from aerie_cli.utils.sessions import get_active_session_client
 
-
 # in case src_path is not from aeri-cli src and from site-packages
 src_path = os.path.abspath(os.path.dirname(os.path.realpath(__file__)) + "/../src")
 sys.path.insert(0, src_path)
@@ -45,17 +44,15 @@ FILES_PATH = os.path.join(TEST_DIR, "files")
 CONFIGURATIONS_PATH = os.path.join(FILES_PATH, "configuration")
 CONFIGURATION_PATH = os.path.join(CONFIGURATIONS_PATH, "localhost_config.json")
 
-@pytest.fixture(scope="session", autouse=True)
-def set_up_environment(request):
-    # Resets the configurations and adds localhost
-    deactivate_session()
-    delete_all_persistent_files()
-    upload_configurations(CONFIGURATION_PATH)
-    activate_session("localhost")
-    persisent_client = None
-    try:
-        persisent_client = get_active_session_client()
-    except:
-        raise RuntimeError("Configuration is not active!")
-    assert persisent_client.host_session.gateway_url == GATEWAY_URL,\
-        "Aerie instances are mismatched. Ensure test URLs are the same."
+# Resets the configurations and adds localhost
+deactivate_session()
+delete_all_persistent_files()
+upload_configurations(CONFIGURATION_PATH)
+activate_session("localhost")
+persisent_client = None
+try:
+    persisent_client = get_active_session_client()
+except:
+    raise RuntimeError("Configuration is not active!")
+assert persisent_client.host_session.gateway_url == GATEWAY_URL,\
+    "Aerie instances are mismatched. Ensure test URLs are the same."
