0b1f984fb308b770a3af10717e0d0c269857112d
Jonathan J Navarro <Jonathan.J.Navarro@jpl.nasa.gov>
2023-08-09 14:24:16 -0700

    Update PersistentConfigurationManager after clean

--- src/aerie_cli/commands/configurations.py
+++ src/aerie_cli/commands/configurations.py
@@ -242,3 +242,5 @@ def delete_all_files(
             return
 
     delete_all_persistent_files()
+    # Update the PersistentConfigurationManager's cached configurations to account for the clean
+    PersistentConfigurationManager.read_configurations()
