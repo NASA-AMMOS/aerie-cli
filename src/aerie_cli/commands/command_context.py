4fb3d9eda1e281a9c424dc4e481a4816621af196
Carter Mak <Carter.Mak@jpl.nasa.gov>
2023-09-06 09:23:15 -0700

    Requre authenticated Aerie session to use admin secret

--- src/aerie_cli/commands/command_context.py
+++ src/aerie_cli/commands/command_context.py
@@ -31,8 +31,10 @@ class CommandContext:
             client = get_active_session_client()
 
         if cls.hasura_admin_secret:
+            if client.aerie_host.aerie_jwt is None:
+                raise RuntimeError(f"Unauthenticated Aerie session")
             client.aerie_host.session.headers["x-hasura-admin-secret"] = cls.hasura_admin_secret
             client.aerie_host.session.headers["x-hasura-role"] = "aerie_admin"
-            client.aerie_host.session.headers["x-hasura-user-id"] = client.aerie_host.aerie_jwt.username if client.aerie_host.aerie_jwt else "hasura_admin"
+            client.aerie_host.session.headers["x-hasura-user-id"] = client.aerie_host.aerie_jwt.username
 
         return client
