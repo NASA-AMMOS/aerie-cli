b709fbae57652f3022bb7b1aad6d78a1163303bc
Parker Abercrombie <parker.abercrombie@jpl.nasa.gov>
2026-05-07 16:16:36 -0700

    Upgrade to Aerie 4.2.1

--- src/aerie_cli/aerie_host.py
+++ src/aerie_cli/aerie_host.py
@@ -13,7 +13,11 @@ COMPATIBLE_AERIE_VERSIONS = [
     "3.7.1",
     "3.8.0",
     "3.8.1",
-    "4.0.0"
+    "4.0.0",
+    "4.1.0",
+    "4.1.1",
+    "4.2.0",
+    "4.2.1"
 ]
 
 class AerieHostVersionError(RuntimeError):
